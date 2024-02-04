700b7cd8a03e1d9eba5aec1e8987634bc2ab205f
Attila Huszár <attila.huszar@outlook.com>
2024-02-04 23:06:13 +0100

    eslint compliance

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,13 +89,15 @@ export default function Intro() {
         <a
           className="borderBlack cursor-pointer rounded-full bg-white p-3 text-[1.65rem] text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
           href="https://www.linkedin.com/in/attilahuszar/"
-          target="_blank">
+          target="_blank"
+          rel="noopener noreferrer">
           <FaLinkedinIn />
         </a>
         <a
           className="borderBlack cursor-pointer rounded-full bg-white  p-3 text-[1.65rem] text-gray-700 transition hover:scale-[1.15] hover:text-gray-950 focus:scale-[1.15] active:scale-105 dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/attila-huszar"
-          target="_blank">
+          href="https://github.com/attila-huszar/"
+          target="_blank"
+          rel="noopener noreferrer">
           <FaGithub />
         </a>
       </motion.div>
