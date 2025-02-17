b65a475683f7bc0837716ea784f3d19136919831
Attila Huszár <attila.huszar@outlook.com>
2025-02-17 17:26:37 +0100

    feat: integrate Vercel Analytics and Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@/components/header'
-import ActiveSectionContextProvider from '@/context/active-section-context'
-import Footer from '@/components/footer'
-import ThemeSwitch from '@/components/theme-switch'
-import ThemeContextProvider from '@/context/theme-context'
 import { Toaster } from 'react-hot-toast'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+import ThemeContextProvider from '@/context/theme-context'
+import ThemeSwitch from '@/components/theme-switch'
+import ActiveSectionContextProvider from '@/context/active-section-context'
+import './globals.css'
+
 import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,10 +23,9 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${inter.className} relative bg-stone-50 pt-32 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="absolute -top-24 right-44 -z-10 size-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
-        <div className="absolute -top-4 left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:-left-60 2xl:-left-20"></div>
-
+        className={`${inter.className} dark:text-opacity-90 relative bg-stone-50 pt-32 text-gray-950 dark:bg-gray-900 dark:text-gray-50`}>
+        <div className="absolute -top-24 right-44 -z-10 size-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="absolute -top-4 left-[-35rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:-left-60 2xl:-left-20 dark:bg-[#676394]"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
@@ -33,6 +35,8 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
