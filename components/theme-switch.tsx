acae9e19d2e722a1f67d5e54ddbaa840dd1e19a1
Attila Huszár <attila.huszar@outlook.com>
2025-04-08 20:29:37 +0200

    feat: ui updates

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -11,7 +11,7 @@ export default function ThemeSwitch() {
       type="button"
       onClick={toggleTheme}
       aria-label="Toggle theme"
-      className="fixed right-5 bottom-5 flex size-12 items-center justify-center rounded-full border border-white/40 bg-white/80 shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950"
+      className="fixed right-5 bottom-5 flex size-12 cursor-pointer items-center justify-center rounded-full border border-white/40 bg-white/80 shadow-2xl backdrop-blur-[0.5rem] transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950"
     >
       {theme === 'light' ? <BsSun /> : <BsMoon />}
     </button>
