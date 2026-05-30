2fb2e26a0de7d860f7d29a9cd57578c469f1aeb2
Attila Huszár <attila.huszar@outlook.com>
2026-05-30 16:36:11 +0200

    fix: update footer styles for consistency

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,13 +2,13 @@ import { RiCopyleftFill } from 'react-icons/ri'
 
 export default function Footer() {
   return (
-    <footer className="m-auto mb-4 max-w-[53rem] px-18 text-xs text-gray-400">
+    <footer className="m-auto mb-4 max-w-212 px-18 text-xs text-gray-400">
       <div className="mb-2 flex items-center justify-center gap-1">
         <p>Attila Huszár</p>
         <RiCopyleftFill />
         <p>{new Date().getFullYear()}</p>
       </div>
-      <p className="text-center text-pretty">
+      <p className="text-center text-balance">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
         Framer Motion & Resend
